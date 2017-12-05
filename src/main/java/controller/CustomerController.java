75e31eacb0d469d73cad56e4c1dc9d5828e31c59
jiriik <vlasimsky.jiri@gmail.com>
2017-12-05 08:31:02 +0100

    Added placeholder for storing customers

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -28,4 +28,11 @@ public class CustomerController {
 
         return "list-customers";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "customer-form";
+    }
 }
